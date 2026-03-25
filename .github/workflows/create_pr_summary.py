8e42d83c966781e1b37c5a5379c846a9b6927cfe
M. Reza Heidari <mohammad.reza.heidari@desy.de>
2026-03-25 17:13:34 +0100

    Update create_pr_summary.py

--- .github/workflows/create_pr_summary.py
+++ .github/workflows/create_pr_summary.py
@@ -2,7 +2,7 @@ import os
 import sys
 from google import genai
 
-echo "Reza exits summarg ..."
+print("Reza exits summarg ...")
 exit(0)
 # Pass the gemini api key
 api_key = os.environ.get('GEMINI_API_KEY')
